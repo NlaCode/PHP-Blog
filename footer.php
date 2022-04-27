bd24749d2cf76025ae85fef4f5c575cc307e4141
André Luiz Nascimento <93333781+NlaCode@users.noreply.github.com>
2022-04-26 22:36:17 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>André Luiz Nascimento &copy; 2022</p>
+</footer>
+</body>
+</html>
---
+++ posts.php
@@ -0,0 +1,32 @@
+<?php
+
+  $posts = [
+    [
+      'id' => 1,
+      'title' => 'Programando com muito café',
+      'description' => 'Neste post você vai aprender tudo sobre vício em café',
+      'tags' => ['Café', 'PHP', 'lógica de programação'],
+      'img' => 'prog-1.jpg'
+    ],
+    [
+      'id' => 2,
+      'title' => 'Orientação a Objetos com Java!',
+      'description' => 'Front End é legal!',
+      'tags' => ['Java', 'OOP', 'Programação Avançada'],
+      'img' => 'prog-2.jpg'
+    ],
+    [
+      'id' => 3,
+      'title' => 'A importância da lógica de programação',
+      'description' => 'Estudar SQL ajudou bastante!',
+      'tags' => ['lógica', 'programação', 'algoritmos'],
+      'img' => 'prog-3.jpg',
+    ],
+    [
+      'id' => 4,
+      'title' => 'Programando em PHP?',
+      'description' => 'Gostei bastante!',
+      'tags' => ['programação', 'PHP', 'lógica de programação'],
+      'img' => 'prog-4.jpg'
+    ],
+  ];
